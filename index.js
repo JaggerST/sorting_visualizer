7a3fe1f575800acb18fe9168d7c3d85be10a1d1b
Jakob <jakob.steinke@stud.tu-darmstadt.de>
2024-01-02 15:39:52 +0100

    Projektdateien hinzufügen.

---
+++ index.js
@@ -0,0 +1,146 @@
+let randomize_array = document.getElementById("randomize_array_btn");
+let sort_btn = document.getElementById("sort_btn");
+let bars_container = document.getElementById("bars_container");
+let select_algo = document.getElementById("algo");
+let speed = document.getElementById("speed");
+let slider = document.getElementById("slider");
+let minRange = 1;
+let maxRange = slider.value;
+let numOfBars = slider.value;
+let heightFactor = 4;
+let speedFactor = 100;
+let unsorted_array = new Array(numOfBars);
+
+slider.addEventListener("input", function () {
+  numOfBars = slider.value;
+  maxRange = slider.value;
+  //console.log(numOfBars);
+  bars_container.innerHTML = "";
+  unsorted_array = createRandomArray();
+  renderBars(unsorted_array);
+});
+
+speed.addEventListener("change", (e) => {
+  speedFactor = parseInt(e.target.value);
+});
+
+let algotouse = "";
+
+select_algo.addEventListener("change", function () {
+  algotouse = select_algo.value;
+});
+
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function createRandomArray() {
+  let array = new Array(numOfBars);
+  for (let i = 0; i < numOfBars; i++) {
+    array[i] = randomNum(minRange, maxRange);
+  }
+
+  return array;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  unsorted_array = createRandomArray();
+  renderBars(unsorted_array);
+});
+
+function renderBars(array) {
+  for (let i = 0; i < numOfBars; i++) {
+    let bar = document.createElement("div");
+    bar.classList.add("bar");
+    bar.style.height = array[i] * heightFactor + "px";
+    bar.style.border = "1px solid #0e1111";
+    bars_container.appendChild(bar);
+  }
+}
+
+randomize_array.addEventListener("click", function () {
+  unsorted_array = createRandomArray();
+  bars_container.innerHTML = "";
+  renderBars(unsorted_array);
+});
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+sort_btn.addEventListener("click", function () {
+    switch (algotouse) {
+      case "bubble":
+        bubbleSort(unsorted_array);
+        break;
+      case "insertion":
+        InsertionSort(unsorted_array);
+        break;
+      default:
+        bubbleSort(unsorted_array);
+        break;
+    }
+  });
+
+
+async function bubbleSort(array) {
+  let bars = document.getElementsByClassName("bar");
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        for (let k = 0; k < bars.length; k++) {
+          if (k !== j && k !== j + 1) {
+            bars[k].style.backgroundColor = "#00FF00";
+          }
+        }
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        bars[j].style.height = array[j] * heightFactor + "px";
+        bars[j].style.backgroundColor = "#0e6b0e";
+        //bars[j].innerText = array[j];
+        bars[j + 1].style.height = array[j + 1] * heightFactor + "px";
+        bars[j + 1].style.backgroundColor = "#0e6b0e";
+        //bars[j + 1].innerText = array[j + 1];
+        await sleep(speedFactor);
+      }
+    }
+    await sleep(speedFactor);
+  }
+  return array;
+}
+
+
+async function InsertionSort(array) {
+  let bars = document.getElementsByClassName("bar");
+  for (let i = 1; i < array.length; i++) {
+    let key = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      bars[j + 1].style.height = array[j + 1] * heightFactor + "px";
+      bars[j + 1].style.backgroundColor = "#0e6b0e";
+      //bars[j + 1].innerText = array[j + 1];
+      await sleep(speedFactor);
+
+      for (let k = 0; k < bars.length; k++) {
+        if (k != j + 1) {
+          bars[k].style.backgroundColor = "#00FF00";
+        }
+      }
+      j = j - 1;
+    }
+    array[j + 1] = key;
+    bars[j + 1].style.height = array[j + 1] * heightFactor + "px";
+    bars[j + 1].style.backgroundColor = "#0e6b0e";
+    //bars[j + 1].innerText = array[j + 1];
+    await sleep(speedFactor);
+  }
+
+  for (let k = 0; k < bars.length; k++) {
+    bars[k].style.backgroundColor = "#00FF00";
+  }
+  return array;
+} 
+
+
